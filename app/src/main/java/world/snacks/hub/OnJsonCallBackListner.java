b07211fcc0b0cf4ceee5b88a032692403f015af3
eSoftwareInfo <88030551+esoftwareinfo@users.noreply.github.com>
2024-07-02 16:51:29 +0530

    Delete app/src/main directory

--- app/src/main/java/world/snacks/hub/OnJsonCallBackListner.java
+++
@@ -1,6 +0,0 @@
-package world.snacks.hub;
-
-public interface OnJsonCallBackListner {
-
-    public void OnJsonDone();
-}
--- app/src/main/java/world/snacks/hub/OnSpleshJsonCallBackListner.java
+++
@@ -1,6 +0,0 @@
-package world.snacks.hub;
-
-public interface OnSpleshJsonCallBackListner {
-
-    public void OnSpleshJsonDone();
-}
